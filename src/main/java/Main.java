26d4b161bd55aebf22ee3d696cb7c522a5b9f35b
AnkurKaushik <a123ankur@yahoo.com>
2021-02-12 15:22:21 -0600

    display options, welcome screen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
 public class Main {
+    private static void welcomeScreen()
+    {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("Welcome to LockedMe!");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("    Created by:");
+        System.out.println("   Ankur Kaushik");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+    }
+    private static void dispOptions()
+    {
+        System.out.println("Select an option from the list below:");
+        System.out.println(" 1. Return Files\n 2. User Interface Details (Add, Delete, Search)\n 3. Close Application");
+    }
     public static void main(String[] args)
     {
-        
+        welcomeScreen();
+        dispOptions();
     }
 }
