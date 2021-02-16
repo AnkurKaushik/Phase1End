2923a6611d7f2147c2800af5365b43ad605a87a7
AnkurKaushik <a123ankur@yahoo.com>
2021-02-16 12:13:41 -0600

    Feb 15th changes, outer loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    static boolean isRunning = true;
+
     private static void welcomeScreen()
     {
         System.out.println("~~~~~~~~~~~~~~~~~~~~");
@@ -13,9 +18,27 @@ public class Main {
         System.out.println("Select an option from the list below:");
         System.out.println(" 1. Return Files\n 2. User Interface Details (Add, Delete, Search)\n 3. Close Application");
     }
+    private static void outerLoop()
+    {
+        do
+        {
+            dispOptions();
+            Scanner sc = new Scanner(System.in);
+            int userChoice = -1;
+            try {
+                userChoice = sc.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("\nPlease input a valid number in the following format (ex:) 2\n");
+            }
+            System.out.println(userChoice);
+            if(userChoice == 3)
+                isRunning = false;
+        } while (isRunning);
+    }
     public static void main(String[] args)
     {
         welcomeScreen();
-        dispOptions();
+        outerLoop();
+
     }
 }
